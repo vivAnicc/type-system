9316ec5b20de110d490cdf60169e0ce9e8218e29
Niccolò <nicc.gemm@gmail.com>
2025-03-21 16:34:45 +0100

    Created data layout to simplify types

--- src/data.zig
+++ src/data.zig
@@ -49,4 +49,32 @@ pub const Layout = union(enum) {
             .Zero => 0,
         };
     }
+
+    pub fn normalize(self: Layout, alloc: std.mem.Allocator) !Layout {
+        return switch (self) {
+            .Repeat => |val| {
+                if (val.amount == 0) {
+                    return .Zero;
+                }
+                if (val.amount == 1) {
+                    return val.layout.*;
+                }
+                const inner = try val.layout.normalize(alloc);
+                if (inner.size() == 0) {
+                    return .Zero;
+                }
+                if (inner == .Repeat) {
+                    return .{ .Repeat = .{
+                        .layout = inner.Repeat.layout,
+                        .amount = val.amount * inner.Repeat.amount,
+                    } };
+                }
+            },
+            .And => |val| self,
+            .Or => |val| self,
+            .Tag => |val| self,
+            .Single => |val| self,
+            .Zero => |val| self,
+        }
+    }
 };
